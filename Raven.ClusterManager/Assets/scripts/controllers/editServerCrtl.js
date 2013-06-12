f9ed39276cb2eaf783f1b257ea05fa83c02ce43a
Alon Eini <alon@ayende.com>
2013-06-12 11:47:27 +0300

    Adding a "Edit a server" option into RavenDB Cluster Manager

---
+++ Raven.ClusterManager/Assets/scripts/controllers/editServerCrtl.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="../main.js" />
+/*global angular:false */
+'use strict';
+clusterManagerApp.controller('editServerCtrl', function editServerCtrl($scope, $http) {
+    $scope.chooseServer = function () {
+
+        $http.get('/api/servers').success(function (result) {
+            $scope.stats = result;
+
+        });
+    };
+    $scope.saveChanges = function () {
+        var changedServer =
+            {
+                id: $scope.selectedServer.id,
+                url: $scope.selectedServer.url,
+                serverName: $scope.selectedServer.serverName,
+                clusterName: $scope.selectedServer.clusterName,
+                isOnline: $scope.selectedServer.isOnline,
+                credentialsId: $scope.selectedServer.credentialsId
+            };
+        $http.put('/api/servers/editServer', changedServer).success(function () {
+            alert('The changes had been saved.');
+        });
+    };
+
+});
