a59d45bacd7dfad02f4c64a79820ab0c3fcb2587
Alon Eini <alon@ayende.com>
2013-06-12 11:43:53 +0300

    Adding a "generate an API key" button in servers.html

---
+++ Raven.ClusterManager/Assets/scripts/controllers/apiCtrl.js
@@ -0,0 +1,59 @@
+﻿/// <reference path="../main.js" />
+/*global angular:false */
+'use strict';
+
+clusterManagerApp.controller('apiCtrl', function apiCtrl($scope, $http) {
+
+    $scope.getNewKey = function () {
+        var newApiKey = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 22; i++)
+            newApiKey += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return newApiKey;
+
+    };
+
+    $scope.generateSecret = function () {
+
+        $scope.Secret = $scope.getNewKey();
+    };
+
+    $scope.doesNotHaveName = function () {
+        return !$scope.name;
+    };
+
+
+    $scope.saveApiKeys = function () {
+        
+        if ($scope.name == null || $scope.Secret == null) {
+            return;
+        }
+        
+        $scope.FullApiKey = $scope.name + '/' + $scope.Secret;
+        var countServer = 0;
+        var apiKeysToSave = []; // Array of class ApiKeyViewModel
+        _.forEach($scope.stats.servers, function (value, index, array) {
+            if (!value.serverCheckbox)
+                return;
+
+            apiKeysToSave.push({ Secret: value.Secret, Name: value.name, Server: value.Id, Enable:$scope.Enable11 });// If checked then get info
+            countServer += 1;// Checking if any server was signed
+        });
+        
+        if (countServer == 0) {
+            $scope.alert = "* Please choose a server";
+            return; // If no server was checked than return
+        }
+        if (countServer != 0)
+            $scope.alert = "";// In case the user signed a textbox
+        
+        $http.post("/api/servers/save-api", apiKeysToSave)
+            .success(function (result) {
+                alert('Done');
+            });
+    };
+
+
+});
--- Raven.ClusterManager/Assets/scripts/controllers/mainCtrl.js
+++ Raven.ClusterManager/Assets/scripts/controllers/mainCtrl.js
@@ -5,7 +5,7 @@
 clusterManagerApp.controller('MainCtrl', function mainCtrl($scope, $http, $timeout) {
     $scope.startDiscovering = function () {
         $scope.isDiscovering = true;
-        $http.get('/api/discovery/start').success(function () {
+        $http.get('/api/discovery/start').success(function () { //calls to MVC controller by http get
             $scope.isDiscovering = false;
         });
     };
@@ -14,7 +14,7 @@ clusterManagerApp.controller('MainCtrl', function mainCtrl($scope, $http, $timeo
     $scope.getStats = function () {
         $http.get('/api/servers').success(function (result) {
             $scope.stats = result;
-            _.forEach($scope.stats.servers, function(value, index, array) {
+            _.forEach($scope.stats.servers, function (value, index, array) {
                 value.cssClass = '';
                 if (value.isOnline) {
                     if (value.isUnauthorized) {
@@ -25,10 +25,53 @@ clusterManagerApp.controller('MainCtrl', function mainCtrl($scope, $http, $timeo
                 } else {
                     value.cssClass += ' error';
                 }
+              
             });
         });
         
         // timeoutPromise = $timeout($scope.getStats, 5000);
     };
     $scope.getStats();
+    $scope.getNewKey = function () {
+        var newApiKey = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 22; i++)
+            newApiKey += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return newApiKey;
+
+    };
+
+    $scope.generateSecret = function () {
+
+        $scope.Secret = $scope.getNewKey();
+    };
+
+    $scope.doesNotHaveName = function () {
+        return !$scope.name;
+    };
+
+
+    $scope.saveApiKeys = function () {
+
+        $scope.FullApiKey = $scope.name + '/' + $scope.Secret;
+
+        var apiKeysToSave = []; // Array of class ApiKeyViewModel
+        _.forEach($scope.stats.servers, function (value, index, array) {
+            if (value.serverCheckbox == false)
+                return;
+
+            apiKeysToSave.push({ Secret: value.Secret, Name: value.name, Server: value.Id });// If checked then get info
+        });
+
+        $http.post('/api/servers/save-api', apiKeysToSave)
+            .success(function (result) {
+                alert('Done');
+            });
+      //  $http.post('/api/servers/createNewServer', serverToSave)
+        //  .success(function (result) {
+          //    alert('Done');
+          //});
+    };
 });
